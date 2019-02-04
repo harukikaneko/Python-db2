1f02d5a316e4dbc245ec54a96897dbbeef8d1c21
HarukiKaneko <niku11298@gmail.com>
2019-02-04 21:55:43 +0900

    Add env file dotenv

--- mrtEnv.py
+++ mrtEnv.py
@@ -1,9 +1,18 @@
-PLN_DBSCHEMA = "GIV001"
-PLN_DBNM = "mrtgivdb"
-PLN_DBUSERNM = "db2inst5"
-PLN_DBPASSWD = "db2inst5"
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 
-IF_DBSCHEMA = "IFMURATA"
-IF_DBNM = "mrtifdb"
-IF_DBUSERNM = "db2inst5"
-IF_DBPASSWD = "db2inst5"
+load_dotenv(verbose=True)
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+PLN_DBSCHEMA = os.environ.get("PLN_DBSCHEMA")
+PLN_DBNM = os.environ.get("PLN_DBNM")
+PLN_DBUSERNM = os.environ.get("PLN_DBUSERNM")
+PLN_DBPASSWD = os.environ.get("PLN_DBPASSWD")
+
+IF_DBSCHEMA = os.environ.get("IF_DBSCHEMA")
+IF_DBNM = os.environ.get("IF_DBNM")
+IF_DBUSERNM = os.environ.get("IF_DBUSERNM")
+IF_DBPASSWD = os.environ.get("IF_DBPASSWD")
